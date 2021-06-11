029347cfa25ab1cfca2ff6a016501188ad76785d
Chiaki Obara <obara@ham.works>
2021-06-11 19:29:03 +0900

    初期値の出力

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { useEffect, useState } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import axios from "axios";
+import "./Home.css";
+import { Todo } from "../types/todo";
 
 const Home: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((todo) => {
+        if (todo.data) {
+          setTodos(todo.data);
+        }
+        console.log(todo.data);
+      });
+  }, []);
+  console.log(todos);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>TODO List</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">TODO List</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        {todos.map((todo: Todo) => (
+          <div key={todo.id}>{todo.title}</div>
+        ))}
       </IonContent>
     </IonPage>
   );
