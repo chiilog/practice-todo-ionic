a9b90aeebf6881ddbf53489be9f9c14bffa0d087
Chiaki Obara <obara@ham.works>
2021-06-11 19:45:43 +0900

    スライド可なコンポーネントに差し替え

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import {
   IonContent,
   IonHeader,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
   IonPage,
   IonTitle,
   IonToolbar,
@@ -38,9 +44,25 @@ const Home: React.FC = () => {
             <IonTitle size="large">TODO List</IonTitle>
           </IonToolbar>
         </IonHeader>
-        {todos.map((todo: Todo) => (
-          <div key={todo.id}>{todo.title}</div>
-        ))}
+        <IonList>
+          {todos.map((todo: Todo) => (
+            <IonItemSliding key={todo.id}>
+              <IonItemOptions side="start">
+                <IonItemOption color="success" expandable>
+                  Complete
+                </IonItemOption>
+              </IonItemOptions>
+              <IonItem>
+                <IonLabel className="ion-text-wrap">{todo.title}</IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption color="danger" expandable>
+                  Delete
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
